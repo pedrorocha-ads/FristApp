99c931f87ce96586d0f9f045ca1cb0139b9a290e
Pedro Rocha <161658584+pedrorocha-ads@users.noreply.github.com>
2025-02-13 18:37:51 -0300

    Setting

---
+++ src/main/java/com/br/FristApp/FristAppApplication.java
@@ -0,0 +1,12 @@
+package com.br.FristApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FristAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FristAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/br/FristApp/FristAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.FristApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FristAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
