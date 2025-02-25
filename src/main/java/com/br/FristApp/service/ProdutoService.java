984cedebbb7a3fcbdb5b54c44311d12139632436
Pedro Rocha <161658584+pedrorocha-ads@users.noreply.github.com>
2025-02-25 18:54:15 -0300

    Construção de Multi Camadas

--- src/main/java/com/br/FristApp/service/ProdutoService.java
+++ src/main/java/com/br/FristApp/service/ProdutoService.java
@@ -1,7 +1,9 @@
 package com.br.FristApp.service;
 
 
+import com.br.FristApp.dto.request.ProdutoRequestDTO;
 import com.br.FristApp.dto.response.ProdutoResponseDTO;
+import com.br.FristApp.model.Produto;
 import com.br.FristApp.repository.ProdutoRepository;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +20,17 @@ public class ProdutoService {
     }
     public List<ProdutoResponseDTO> listarProdutos(){ //lista os produtos
         return produtoRepository.findAll().stream()
-                .map(produto -> new ProdutoResponseDTO(produto.getId(), produto.getNome(), produto.getPreco(), produto.getNumeroSerie()))
+                .map(produto -> new ProdutoResponseDTO(produto.getId(), produto.getNome(), produto.getPreco()))
                 .collect(Collectors.toList()); // collection é uma interface que representa um grupo de objetos para que o java possa manipular
+    }
+    public ProdutoResponseDTO buscarPorID(Long id){ //busca o produto pelo id
+        Produto produto =  produtoRepository.findByIdd(id)
+                .orElseThrow(() -> new RuntimeException("Produto não encontrado"));//caso não encontre o produto, retorna uma exceção
+        return new ProdutoResponseDTO(produto.getId(), produto.getNome(), produto.getPreco());//retorna o produto pelo id, nome e preço
+    }
+    public ProdutoResponseDTO criarProduto(ProdutoRequestDTO produtoRequestDTO){
+        Produto novoProduto = new Produto(null, produtoRequestDTO.getNome(), produtoRequestDTO.getPreco(), produtoRequestDTO.getNumeroSerie()); //cria um novo produto
+        Produto produtoSalvo = produtoRepository.save(novoProduto); //salva o produto
+        return new ProdutoResponseDTO(produtoSalvo.getId(), produtoSalvo.getNome(), produtoSalvo.getPreco()); //retorna o produto salvo
     }
 }
